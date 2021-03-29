1b998ed5a4f94014ea8aa04c9c63950534cd17d3
Rockstar RyuCoder <rockstar.ryucoder@gmail.com>
2021-03-29 20:52:10 +0530

    Filenames Generated.

--- main.py
+++ main.py
@@ -8,31 +8,49 @@ START = settings.CURRENT_YEAR
 STOP = settings.CURRENT_YEAR - settings.YEAR_RANGE
 RANGE = -1
 
-all_data = []
+filenames = []
 
-for dairy_number in range(1, (settings.DAIRY_RANGE + 1)):
-    for year_number in range(START, STOP, RANGE):
+for dairy_number in range(1, 3):
+    for year_number in range(START, 2023):
 
         response = requests.get(settings.CAPTCHA_URL)
         captcha_number = response.json()
+
+        print()
+        print()
+        print("response")
+        print(response)
+        
+        print(response.text)
+        print(type(response.text))
+
+        pprint(dir(response))
+
+        print(response.content)
+        print(type(response.content))
+
+        print()
+        print()
 
         data = {
             "d_no": dairy_number,
             "d_yr": year_number,
             "ansCaptcha": captcha_number 
         }
-        print() 
-        print() 
-        pprint(data) 
-        print() 
-        print()
-
-        all_data.append(data)
 
 
-print() 
-print() 
-pprint("all_data") 
-pprint(len(all_data)) 
-print() 
+        # store the response to the output folder 
+
+        filename = f"{settings.OUTPUT_FOLDER}/{year_number}/{dairy_number}"
+        
+        filenames.append(filename)
+
+        # with open(filename, "wb") as f:
+        #     f.write(response.content)
+
+print()
+print()
+print("filenames")
+pprint(filenames)
+print()
 print()
